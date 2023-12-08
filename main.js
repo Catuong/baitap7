21246d629a38aa535b2600716d123fa762dc4be6
Catuong <148960507+Catuong@users.noreply.github.com>
2023-12-08 16:56:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+// 1.
+// const numbers = [1, 2, 3, 4, 5, 6];
+// function getEvenNumbers(array) {
+//     const count = [];
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i] % 2 === 0) {
+//         cart.push(array[i]);
+//       }
+//     }
+//     return count;
+//   }
+//   console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+
+// 2. 
+// const numbers = [1, 3, 6, 8, 10];
+//   console.log(numbers.length);
+
+
+3.
+// const numbers = [5, 1, 9, 3, 10, 4];
+// function maxNumber(array){
+//     let max = array[0];
+//     let count = 0; 
+//     for( let i = 0;  i < array.length; i++ ){
+//         if(max < array[i]){
+//             max = array[i];
+//         }
+//     }
+
+// console.log(max);
+//     return 0;
+// }
+// console.log(maxNumber([5, 1, 9, 3, 10, 4]));
+
+
+4.
+// const numbers = [5, 1, 9, 3, 10, 4];
+// function maxNumber(array){
+//     let max = array[0];
+//     let cart = 0; 
+//     for( let i = 0;  i < array.length; i++ ){
+//         if(max > array[i]){
+//             max = array[i];
+//         }
+//     }
+// console.log(max);
+//     return 0;
+// }
+// console.log(maxNumber([5, 1, 9, 3, 10, 4]));
+
+
+// 5. 
+// function check_arr(arr){
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i ++){
+//         if (arr[i] == 5)  {
+//             count ++;
+//         }
+//     }
+//  return (count >0) 
+// }
+// let arr = [1, 3, 5];
+
+// console.log(check_arr("5", arr));
+
+// 6. 
+// const num = [1, 2, 3];
+// console.log(num.join(''));
+// 6. 
+// const num = [1, 2, 3];
+// const str = "" + 123; 
+// console.log(str); 
+7. 
+const num = [1, 2, 3]; 
+
+function changeStr(arr){
+let str = []; 
+for(let i = 0; i < arr.length; i++){
+  str.toString(arr[i]);
+}
+console.log(str);
+}
+
+console.log(changeStr([1, 2, 3])); 
+// str.toString(); 
+
+
+// 8. 
+// // const numbers = [1, 2, 3];
+// // function sumArray(arr){
+// //     let sum = 0;
+// //     for (let i = 0; i < arr.length; i++){
+// //         sum += arr[i];
+// //     }
+     
+// //     return sum;
+// // }
+// // console.log(sumArray([1, 2, 3]));
+
+
+// // function findX(haystack,needle) {
+// //     for(const item of haystack) {
+// //        if(item === needle) {
+// //           return true;
+// //        }
+// //     }
+// //     return false;
+// //  }
+// //  let names = [1, 3, 5];
+// //  console.log(findX(names,5));
+
+// 9. 
+// let arr = [1, 2, 3, 4, 5];
+//     for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === 2) {
+//         arr.splice(i, 1);
+//     }
+// }
+// console.log(arr);
+
+// const number = [1,3,5];
+
+
+// 10. 
+// // const numbers1 = [1, 2, 3];
+// // const numbers2 =  [4, 5, 6];
+// // console.log(numbers1.concat(numbers2))
+
+
+
+
+
+
+
+
+
+
